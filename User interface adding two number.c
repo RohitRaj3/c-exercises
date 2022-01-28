b25d7c7a5a66c31b686fb24cb44ca62d337e64fe
Rohit Raj <53875362+RohitRaj3@users.noreply.github.com>
2022-01-28 14:00:29 +0530

    User interface adding two number
    here is a glitch in this code even if you type any alphabet A-Z it will show adding value without giving you and error

---
+++ User interface adding two number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c=0;
+    
+    printf("enter first number:");
+    scanf("%d",&a);
+    
+    printf("enter second number: ");
+    scanf("%d",&b);
+    
+    c= a+b;
+    printf("%d",c);
+
+    return 0;
+}
